2ca7933f190ee7495145da52d6f2f8b5877cdcea
AliButaev <alee@caspiy.net>
2025-06-16 10:57:23 +0300

    Initial commit

---
+++ PrevQSOFrm/PrevQSOFrm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace DXLog.net
+{
+    partial class PrevQSOForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader IdHeader;
+        private System.Windows.Forms.ColumnHeader QSOIdHeader;
+        private System.Windows.Forms.ColumnHeader FreqHeader;
+        private System.Windows.Forms.ColumnHeader BandHeader;
+        private System.Windows.Forms.ColumnHeader ModeHeader;
+        private System.Windows.Forms.ColumnHeader TimeHeader;
+        private System.Windows.Forms.ColumnHeader CallsignHeader;
+        private System.Windows.Forms.ColumnHeader SentHeader;
+        private System.Windows.Forms.ColumnHeader RcvdHeader;
+        private System.Windows.Forms.ColumnHeader PointsHeader;
+        private System.Windows.Forms.ColumnHeader StnHeader;
+        private System.Windows.Forms.ColumnHeader MultHeader;
+        private System.Windows.Forms.ColumnHeader OperatorHeader;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ColumnHeader DateHeader;
+    }
+}
